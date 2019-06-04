e5ec0059fed75f0d1e043180105227bb504228ca
Laisky <ppcelery@gmail.com>
2019-06-04 13:29:38 +0800

    feat(paas-361): replace iris by gin

--- go.mod
+++ go.mod
@@ -4,15 +4,16 @@ go 1.12
 
 require (
 	github.com/99designs/gqlgen v0.8.3
-	github.com/Laisky/go-utils v1.2.3
+	github.com/Laisky/go-utils v1.3.9-0.20190604052253-ffe7e5430644
 	github.com/Laisky/zap v1.9.2
+	github.com/gin-contrib/pprof v1.2.0
+	github.com/gin-gonic/gin v1.4.0
 	github.com/gomarkdown/markdown v0.0.0-20190222000725-ee6a7931a1e4
-	github.com/iris-contrib/middleware v0.0.0-20181021162728-8bd5d51b3c2e
-	github.com/kataras/iris v11.1.1+incompatible
 	github.com/pkg/errors v0.8.1
-	github.com/prometheus/client_golang v0.9.2
 	github.com/spf13/pflag v1.0.3
 	github.com/spf13/viper v1.1.1
 	github.com/vektah/gqlparser v1.1.2
 	gopkg.in/mgo.v2 v2.0.0-20180705113604-9856a29383ce
 )
+
+replace github.com/ugorji/go v1.1.4 => github.com/ugorji/go/codec v0.0.0-20190204201341-e444a5086c43
--- server.go
+++ server.go
@@ -1,60 +1,58 @@
 package laisky_blog_graphql
 
 import (
-	irisMiddlewares "github.com/Laisky/go-utils/iris-middlewares"
+	"net/http"
+
+	ginMiddlewares "github.com/Laisky/go-utils/gin-middlewares"
+
+	"github.com/gin-contrib/pprof"
+	"github.com/gin-gonic/gin"
 
 	"github.com/99designs/gqlgen/handler"
-	prometheusMiddleware "github.com/iris-contrib/middleware/prometheus"
-	"github.com/kataras/iris"
-	"github.com/kataras/iris/middleware/pprof"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
 
 	utils "github.com/Laisky/go-utils"
 	"github.com/Laisky/zap"
 )
 
 var (
-	server = iris.New()
-	auth   *irisMiddlewares.Auth
+	server = gin.New()
+	auth   *ginMiddlewares.Auth
 )
 
-func setupAuth() {
-	cfg := irisMiddlewares.AuthCfg
-	cfg.Secret = utils.Settings.GetString("settings.secret")
-	auth = irisMiddlewares.NewAuth(cfg)
+func setupAuth() (err error) {
+	cfg := ginMiddlewares.NewAuthCfg(utils.Settings.GetString("settings.secret"))
+	auth, err = ginMiddlewares.NewAuth(cfg)
+	return
 }
 
 func RunServer(addr string) {
-	setupAuth()
-	server.UseGlobal(LoggerMiddleware)
+	if !utils.Settings.GetBool("debug") {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	if err := setupAuth(); err != nil {
+		utils.Logger.Panic("try to setup auth got error", zap.Error(err))
+	}
+	server.Use(LoggerMiddleware)
 
-	server.Any("/health", func(ctx iris.Context) {
-		ctx.Write([]byte("Hello, World"))
+	server.Any("/health", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "hello, world")
 	})
 
-	m := prometheusMiddleware.New("serviceName", 0.3, 1.2, 5.0)
-	server.Use(m.ServeHTTP)
 	// supported action:
-	// cmdline, profile, symbol, goroutine, heap, threadcreate, debug/block
-	server.Any("/pprof/{action:path}", pprof.New())
-	server.Get("/metrics", iris.FromStd(promhttp.Handler()))
+	// cmdline, profile, symbol, goroutine, heap, threadcreate, block
+	pprof.Register(server, "pprof")
 
-	server.Handle("ANY", "/ui/", irisMiddlewares.FromStd(handler.Playground("GraphQL playground", "/graphql/query/")))
-	server.Handle("ANY", "/query/", irisMiddlewares.FromStd(handler.GraphQL(NewExecutableSchema(Config{Resolvers: &Resolver{}}))))
+	server.Any("/ui/", ginMiddlewares.FromStd(handler.Playground("GraphQL playground", "/graphql/query/")))
+	server.Any("/query/", ginMiddlewares.FromStd(handler.GraphQL(NewExecutableSchema(Config{Resolvers: &Resolver{}}))))
 
 	utils.Logger.Info("listening on http", zap.String("addr", addr))
-	utils.Logger.Panic("httpServer exit", zap.Error(server.Run(iris.Addr(addr), iris.WithConfiguration(iris.Configuration{
-		DisablePathCorrection:            false,
-		DisablePathCorrectionRedirection: true,
-	}))))
+	utils.Logger.Panic("httpServer exit", zap.Error(server.Run(addr)))
 }
 
-const IrisCtxKey = "irisctx"
-
-func LoggerMiddleware(ctx iris.Context) {
+func LoggerMiddleware(ctx *gin.Context) {
 	utils.Logger.Debug("request",
-		zap.String("path", ctx.Path()),
-		zap.String("method", ctx.Method()),
+		zap.String("path", ctx.Request.RequestURI),
+		zap.String("method", ctx.Request.Method),
 	)
 	ctx.Next()
 }
