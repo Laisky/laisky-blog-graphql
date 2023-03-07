998c09e6834cdf84724b59f7d7af0b04226074a7
Laisky.Cai <github@laisky.com>
2023-03-07 03:01:04 +0000

    fix: v2

--- internal/web/server.go
+++ internal/web/server.go
@@ -57,6 +57,7 @@ func RunServer(addr string) {
 	})
 	server.Any("/ui/", ginMw.FromStd(playground.Handler("GraphQL playground", "/query/")))
 	server.Any("/query/", ginMw.FromStd(h.ServeHTTP))
+	server.Any("/query/v2/", ginMw.FromStd(h.ServeHTTP))
 
 	log.Logger.Info("listening on http", zap.String("addr", addr))
 	log.Logger.Panic("httpServer exit", zap.Error(server.Run(addr)))
