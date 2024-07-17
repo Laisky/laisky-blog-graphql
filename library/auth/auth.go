a62c47b35c17a2b64926eb80fa7003599b6d0535
Laisky.Cai <github@laisky.com>
2024-07-17 06:29:38 +0000

    refactor: Add authentication middleware and user context to auth package
    This commit refactors the `auth` package to add an authentication middleware and user context. The changes include:
    
    - Implementing the `AuthMw` middleware function to handle user authentication and authorization.
    - Adding the `CtxKeyAuthUser` constant to define the key for the authenticated user in the `gin.Context`.
    - Updating the `Initialize` function to initialize the `Instance` of the `ginMw.Auth` struct.
    - Implementing the `GetUserFromCtx` function to retrieve the authenticated user from the `gin.Context`.
    
    These changes improve the security and usability of the `auth` package by providing a standardized way to handle user authentication and access control.

--- library/auth/auth.go
+++ library/auth/auth.go
@@ -2,12 +2,44 @@
 package auth
 
 import (
+	"net/http"
+
 	ginMw "github.com/Laisky/gin-middlewares/v5"
+	"github.com/Laisky/laisky-blog-graphql/library/jwt"
+	"github.com/gin-gonic/gin"
 )
 
+// Instance global auth instance
 var Instance *ginMw.Auth
 
+const (
+	// CtxKeyAuthUser key for auth user in gin.Context
+	CtxKeyAuthUser string = "authenticated_user"
+)
+
+// Initialize initialize auth
 func Initialize(secret []byte) (err error) {
 	Instance, err = ginMw.NewAuth(secret)
 	return err
+}
+
+// AuthMw gin middleware for auth
+func AuthMw(ctx *gin.Context) {
+	user := new(jwt.UserClaims)
+	if err := Instance.GetUserClaims(ctx, user); err != nil {
+		ctx.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": err.Error()})
+		return
+	}
+
+	ctx.Set(string(CtxKeyAuthUser), user)
+	ctx.Next()
+}
+
+// GetUserFromCtx get user from gin.Context
+func GetUserFromCtx(ctx *gin.Context) *jwt.UserClaims {
+	if user, ok := ctx.Get(CtxKeyAuthUser); ok {
+		return user.(*jwt.UserClaims)
+	}
+
+	return nil
 }
