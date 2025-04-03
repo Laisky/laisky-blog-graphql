7f2aa8ac3550eac89f80590aece98ed82cca2c31
Laisky.Cai <github@laisky.com>
2025-04-03 06:19:53 +0000

    feat: Implement Telegram notes data model and search functionality

---
+++ internal/web/telegram/model/notes.go
@@ -0,0 +1,55 @@
+// Package model defines the data model for the blog service.
+package model
+
+import (
+	"crypto/sha256"
+	"encoding/hex"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
+// TelegramNote is the model for telegram notes
+type TelegramNote struct {
+	ID        primitive.ObjectID    `bson:"_id,omitempty" json:"mongo_id"`
+	CreatedAt time.Time             `bson:"created_at" json:"created_at"`
+	UpdatedAt time.Time             `bson:"updated_at" json:"updated_at"`
+	Content   string                `bson:"content" json:"content"`
+	PostID    int                   `bson:"post_id" json:"post_id"`
+	ArweaveID string                `bson:"arweave_id" json:"arweave_id"`
+	Digest    string                `bson:"digest" json:"digest"`
+	History   []TelegramNoteHistory `bson:"history" json:"history"`
+}
+
+// TelegramNoteHistory is the model for telegram note history
+type TelegramNoteHistory struct {
+	Content   string    `bson:"content" json:"content"`
+	CreatedAt time.Time `bson:"created_at" json:"created_at"`
+	Digest    string    `bson:"digest" json:"digest"`
+}
+
+// UpdateNote update note content
+func (n *TelegramNote) UpdateNote(content string) {
+	// check current content's digest
+	newHashed := sha256.Sum256([]byte(content))
+	newDigest := hex.EncodeToString(newHashed[:])
+
+	if n.Digest == "" {
+		hashed := sha256.Sum256([]byte(n.Content))
+		n.Digest = hex.EncodeToString(hashed[:])
+	}
+
+	if n.Digest == newDigest {
+		return
+	}
+
+	// update note
+	n.History = append(n.History, TelegramNoteHistory{
+		Content:   n.Content,
+		CreatedAt: n.UpdatedAt,
+		Digest:    n.Digest,
+	})
+	n.Content = content
+	n.UpdatedAt = time.Now()
+	n.Digest = newDigest
+}
